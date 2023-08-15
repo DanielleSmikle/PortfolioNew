cd4c0de2aeb3acfba4a979efd16b70bbca1ea361
DanielleSmikle <dsmikle97@gmial.com>
2023-08-15 15:47:52 -0400

    ready for deployment

--- new-portfolio/src/components/header/Header.jsx
+++ new-portfolio/src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
-import ME from '../../assets/dan.png' 
+import ME from '../../assets/dani.png' 
 import HeaderSocial from './HeaderSocials'
 
 
@@ -9,14 +9,16 @@ const Header = () => {
   return (
     <header>
       <div className=".container.header__container">
-        <h5>Hello I'm</h5>
+
+      <div className="header__text"><h5>Hello I'm</h5>
         <h1> Danielle Smikle</h1>
         <h5 className="text-light">Fullstack Developer</h5>
+      </div>
         <CTA />
         <HeaderSocial />
 
         <div className="me">
-          <img src={ME} alt="me"/>
+          <img className= "header__pic" src={ME} alt="me"/>
         </div>
 
           <a href = "#contact" className="scroll__down">Scroll Down</a>
